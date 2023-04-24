51978e62dae28e21429d1b5a7dc62a48f2750740
louisrosch <louis@mail.ch>
2023-04-24 18:22:41 +0200

    metrics

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from matplotlib import pyplot as plt
 
 from utils.Encoders import Encoder
-from utils.approximation import test_filename, compress_img, get_nearest_quality
+from utils.approximation import  compress_img, get_nearest_quality
 from utils.imgtools import get_image_size
-
+from utils.metrics import get_metrics
 if __name__ == '__main__':
     ## draw a plot filesize per quality per encoder
     # i = [i * 0.1 for i in range(1, 1001)]
@@ -12,16 +12,17 @@ if __name__ == '__main__':
     #              [compress_img(test_filename, encoder, quality * 0.01) for quality in i],
     #              label=encoder)
     # plt.legend()
-    # plt.show()
+    # # plt.show()
     test_file = "images/example.png"
-    fsize = get_image_size(test_file)
-    print(fsize)
-
-    # target: 1/2 of original filesize
-    target = fsize / 20
-    qual = get_nearest_quality(target, Encoder.JP2, test_file)
-    print(qual)
-    print(f"Target {target} result {compress_img(test_file, Encoder.JPG,qual )} ")
+    # fsize = get_image_size(test_file)
+    # print(fsize)
+    # # target: 1/2 of original filesize
+    # target = fsize / 20
+    # qual = get_nearest_quality(target, Encoder.JP2, test_file)
+    # print(qual)
+    # print(f"Target {target} result {compress_img(test_file, Encoder.JPG,qual )} ")
+    for i in range(1, 100):
+        print(f"Quality {i} result {compress_img(test_file, Encoder.JXR, i)} ")
 
 
 
---
+++ utils/metrics.py
@@ -0,0 +1,52 @@
+import pydantic
+import sewar
+import cv2
+
+
+
+
+def get_metrics(img1_path, img2_path):
+    """
+    SSIM, SCC, PSNR, ERGAS,
+    mse
+    rmse
+    psnr
+    rmse_sw
+    uqi
+    ssim
+    ergas
+    scc
+    rase
+    sam
+    msssim
+    vifp
+    psnrb
+    :param img1_path:
+    :param img2_path:
+    :return:
+    """
+    img1 = cv2.imread(img1_path)
+    img2 = cv2.imread(img2_path)
+    return {
+        "mse": sewar.full_ref.mse(img1, img2),
+        "rmse": sewar.full_ref.rmse(img1, img2),
+        "psnr": sewar.full_ref.psnr(img1, img2),
+        "uqi": sewar.full_ref.uqi(img1, img2),
+        "ssim": sewar.full_ref.ssim(img1, img2),
+        "ergas": sewar.full_ref.ergas(img1, img2),
+        "scc": sewar.full_ref.scc(img1, img2),
+        "rase": sewar.full_ref.rase(img1, img2),
+        "sam": sewar.full_ref.sam(img1, img2),
+        "msssim": sewar.full_ref.msssim(img1, img2),
+        "vifp": sewar.full_ref.vifp(img1, img2),
+    }
+
+
+
+
+if __name__ == '__main__':
+    example_img_path = "../images/example.png"
+    example_img = cv2.imread(example_img_path)
+    #print(sewar.full_ref.ssim(example_img, example_img))
+    print(get_metrics(example_img_path, example_img_path))
+    pass
