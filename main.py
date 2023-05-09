525cff6282e4e4337f3d923bdac08ffca37921f7
louisrosch <louis@mail.ch>
2023-05-09 21:51:07 +0200

    refactoring, blabla

--- main.py
+++ main.py
@@ -6,11 +6,17 @@ from utils.approximation import compress_img, get_nearest_quality, decode_img
 from utils.imgtools import get_image_size
 from utils.metrics import get_metrics
 
+def get_target_values(files):
+    """
+
+    :param files: Array of image paths
+    :return: Json of target values
+    """
+    res_dict = {}
+    for f in files:
+        res_dict[f] = get_target_qualities(f)
+    return res_dict
+
 if __name__ == "__main__":
-    test_file = "images/example.png"
-    df = get_target_qualities(
-        test_file,
-        target_jpeg_qualities=[40, 50],
-        experiment_encoders=[Encoder.JP2, Encoder.HEIF],
-    )
-    print(df)
+    import glob
+    print(get_target_values(glob.glob("images/*.png")))
